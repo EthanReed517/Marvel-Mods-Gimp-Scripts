37ab5aa0f4912fd18e6833408ea5bc2a33e1c9f1
EthanReed517 <ethanreed517@gmail.com>
2022-08-26 13:53:33 -0500

    Scale Quarter Size script

--- Utilities/MarvelMods-Common-Scale_Quarter.scm
+++ Utilities/MarvelMods-Common-Scale_Quarter.scm
@@ -1,5 +1,5 @@
-; Scales image to half its original size
-(define (script-fu-mua-xml2-scale-half image)
+; Scales image to a quarter of its original size
+(define (script-fu-mua-xml2-scale-quarter image)
 	(gimp-image-undo-group-start image)
 	(gimp-selection-none image)
 	(let*
@@ -7,16 +7,16 @@
 			(newWidth (car (gimp-image-width image)))
 			(newHeight (car (gimp-image-height image)))
 		)
-		(gimp-image-scale image (/ newWidth 2) (/ newHeight 2))
+		(gimp-image-scale image (/ newWidth 4) (/ newHeight 4))
 	)
 	(gimp-displays-flush)
 	(gimp-image-undo-group-end image)
 )
 ; populate script registration information
 (script-fu-register 
-    "script-fu-mua-xml2-scale-half"
-    "Scale to Half Size"
-    "Scales an image to half its size."
+    "script-fu-mua-xml2-scale-quarter"
+    "Scale to Quarter Size"
+    "Scales an image to quarter its size."
     "BaconWizard17"
     "BaconWizard17"
     "August 2022"
@@ -25,4 +25,4 @@
     SF-DRAWABLE     "Layer"       0
 )
 ; register the script within gimp menu
-(script-fu-menu-register "script-fu-mua-xml2-scale-half" "<Image>/Marvel Mods/Utilities")
+(script-fu-menu-register "script-fu-mua-xml2-scale-quarter" "<Image>/Marvel Mods/Utilities")
