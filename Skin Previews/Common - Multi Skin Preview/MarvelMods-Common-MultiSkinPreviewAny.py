715e42dea7f87aef415748426230d422847177c5
EthanReed517 <ethanreed517@gmail.com>
2022-12-31 00:04:27 -0600

    Create custom size multi skin preview script
    Create a multi skin preview script with sliders to customize the size. This is a new Python-Fu script. Old scripts have not been replaced yet.

---
+++ Skin Previews/Common - Multi Skin Preview/MarvelMods-Common-MultiSkinPreviewAny.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gimpfu import*
+
+def multiSkinPreviewAny (theImage, theLayer, columns, rows):
+    # Establish the dimensions of the image based on the number of rows and columns
+    theImageWidth = 543 * columns
+    theImageHeight = 1080 * rows
+    # Create the image
+    theImage = pdb.gimp_image_new(theImageWidth, theImageHeight, 0)
+    # Create the main layer
+    theLayer = pdb.gimp_layer_new(theImage, theImageWidth, theImageHeight, 0, "Background", 100, 28)
+    # Add the layer to the image
+    pdb.gimp_image_add_layer(theImage, theLayer, 0)
+    # Add guides at the top left and bottom right
+    guide = pdb.gimp_image_add_vguide(theImage, 0) 
+    guide = pdb.gimp_image_add_hguide(theImage, 0)
+    guide = pdb.gimp_image_add_vguide(theImage, theImageWidth) 
+    guide = pdb.gimp_image_add_hguide(theImage, theImageHeight)
+    i = 0
+    # Add 1 vertical guide for every column
+    while i < columns:
+        xcoord = i * 543
+        guide = pdb.gimp_image_add_vguide(theImage, xcoord)
+        i += 1
+    # Re-initiate the counter
+    i = 0
+    # Add 1 horizontal guide for every row
+    while i < rows:
+        ycoord = i * 1080
+        guide = pdb.gimp_image_add_hguide(theImage, ycoord)
+        i += 1
+    # Display the new image
+    display = pdb.gimp_display_new(theImage)
+
+register(
+    "python_fu_marvelmods_common_multiskinpreviewany",
+    "Create template grid for skin preview images.",
+    "Create template grid for skin preview images.",
+    "BaconWizard17",
+    "BaconWizard17",
+    "December 2022",
+    "<Image>/Marvel Mods/Skin Previews/Multi Skin Showcase/Custom Size Skin Preview",
+    "",
+    [
+        (PF_SLIDER, "columns", "Number of Columns:", 2, (1, 8, 1)),
+        (PF_SLIDER, "rows", "Number of Rows:", 2, (1, 8, 1))
+    ],
+    [],
+    multiSkinPreviewAny)
+
+main()
