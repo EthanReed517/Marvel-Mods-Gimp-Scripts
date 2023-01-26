653eebb47ae1d2af0193f16955a683e72e2492a0
EthanReed517 <ethanreed517@gmail.com>
2023-01-25 21:29:11 -0600

    XML2 Preview Crop - Copy from MUA1

---
+++ Skin Previews/Crop Screenshots - XML2/Crop Preview for XML2 Character Select Portrait/MarvelMods-XML2-PreviewCSP.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ########### #
+# INFORMATION #
+# ########### #
+# GIMP plugin to crop a mannequin preview for the PC version of Marvel - Ultimate Alliance.
+# This was designed with the intention to use it with modding processes for MarvelMods.com, though it can have other uses. 
+# For detailed instructions, please reference the README.md file included with this download.
+# (c) BaconWizard17 2023
+#
+#   History:
+#   v1.0: 23Jan2023: First published version.
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+# ####### #
+# IMPORTS #
+# ####### #
+# Import the gimpfu module so that scripts can be executed
+from gimpfu import*
+
+
+# ######## #
+# FUNCTION #
+# ######## #
+# Define the operation
+def previewMannequin (image, layer):
+    # Start an undo group so that the entire operation can be undone at once
+    pdb.gimp_image_undo_group_start(image)
+    # Clear the selection (This is done just in case there is a selection, but there shouldn't be)
+    pdb.gimp_selection_none(image)
+    # Crop the image accordingly
+    pdb.gimp_image_resize(image, 493, 981, -713, 0)
+    # Resize the layer to the image size
+    pdb.gimp_layer_resize_to_image_size(layer)
+    # Display the changes
+    pdb.gimp_displays_flush()
+    # End the undo group
+    pdb.gimp_image_undo_group_end(image)
+
+# ######## #
+# REGISTER #
+# ######## #
+# Register the script in GIMP
+register(
+    "python_fu_marvelmods_mua1_previewMannequin",
+    "Crops the preview window for MUA1 mannequins.",
+    "Crops the preview window for MUA1 mannequins.",
+    "BaconWizard17",
+    "BaconWizard17",
+    "January 2023",
+    "Crop Mannequin Preview",
+    "*",
+    [
+        (PF_IMAGE, "image", "Input image", None),
+        (PF_DRAWABLE, 'drawable', 'Layer, mask or channel', None)
+    ],
+    [],
+    previewMannequin,
+    menu='<Image>/Marvel Mods/Skin Previews/Crop Screenshots - MUA1'
+)
+
+
+# ############## #
+# MAIN EXECUTION #
+# ############## #
+main()
---
+++ Skin Previews/Crop Screenshots - XML2/Crop Preview for XML2 Skin/README.md
@@ -0,0 +1,18 @@
+# Marvel Mods GIMP Plugins
+by BaconWizard17
+## Crop MUA1 Skin Preview
+This plugin crops a screenshot from MUA1 PC to create a preview of a conversation portrait (HUD).
+
+### Compatibility
+This script will only work correctly if the resolution is set to 1920x1080. The screenshot must be taken in the character select screen while viewing the character details. I generally take my screenshots with the character in the second slot while viewing their skins. I use the original (v1.0-1.5) 36 Roster Hack team stage as the background
+
+### Installation
+ 1. This script can be installed using `runUpdateAdmin.bat` from my scripts, or it can be placed individually in your GIMP plugins directory. The standard location for this is `C:\Users\(your user)\AppData\Roaming\GIMP\2.10\plug-ins`. Check the main `README.md` file in this project for more details on proper installation.
+
+### Usage
+1. Start with an image open in GIMP that you would like to crop. See the "Compatibility" section above for details on the image type.
+2. In the toolbar, go to `Marvel Mods/Skin Previews/Crop Screenshots - MUA1` and choose the `Crop Skin Preview` plugin.
+3. The image will be cropped to the correct size.
+
+## Credits
+- BaconWizard17: Script creation
