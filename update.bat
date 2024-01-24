1d1198017917fc7d572661d770438b29b8aa4982
EthanReed517 <ethanreed517@gmail.com>
2024-01-24 10:26:08 -0600

    Add error checking to installer

--- update.bat
+++ update.bat
@@ -10,22 +10,46 @@ REM ******************************
 REM * Section 1 - Main Execution *
 REM ******************************
 REM Do not modify anything below this line!
-REM move to the folder of the script
-cd %~dp0
+REM Verify that a folder was entered
+if not %appDataPluginFolder%=="" (
+	REM a folder was entered
+	REM verify that the folder exists
+	if exist %appDataPluginFolder% (
+		REM the folder exists
+		REM move to the folder of the script
+		cd %~dp0
 
-REM copy scheme scripts to the "scripts" folder in GIMP's installation
-REM echo Copying Scheme plugins . . .
-REM for /r %%x in (*.scm) do (
-REM 	copy >nul "%%x" %installScriptsFolder%
-REM )
+		REM copy scheme scripts to the "scripts" folder in GIMP's installation
+		REM echo Copying Scheme plugins . . .
+		REM for /r %%x in (*.scm) do (
+		REM 	copy >nul "%%x" "%installScriptsFolder%"
+		REM )
 
-REM copy python plugins to the "plugins" folder in GIMP's AppData folder
-echo Copying Python plugins . . .
-for /r %%x in (*.py) do (
-	copy >nul "%%x" %appDataPluginFolder%
+		REM copy python plugins to the "plugins" folder in GIMP's AppData folder
+		echo Copying Python plugins . . .
+		for /r %%x in (*.py) do (
+			copy >nul "%%x" %appDataPluginFolder%
+		)
+		
+		REM announce completion
+		echo.
+		echo Installation of plugins is complete.
+	) else (
+		REM the folder does not exist
+		REM display the error message
+		echo ERROR: The folder %appDataPluginFolder% does not exist. Please enter a valid folder name.
+		REM display lack of completion
+		echo.
+		echo Nothing was installed.
+	)
+) else (
+	REM a folder was not entered
+	REM display the error message
+	echo ERROR: The location of the "plugins" folder was not entered. Please edit line 6 of "update.bat" to add this information.
+	REM display lack of completion
+	echo.
+	echo Nothing was installed.
 )
 
-REM pause to show completion
-echo.
-echo Installation of plugins is complete.
+REM pause to show status
 pause
