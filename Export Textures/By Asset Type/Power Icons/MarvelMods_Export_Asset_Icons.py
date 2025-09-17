b2149ecdfa9670321f727e4ffdafd1cbb970be5e
EthanReed517 <ethanreed517@gmail.com>
2025-09-16 20:42:55 -0500

    Power Icons
    Rewrite the power icons script to the new format.

--- Export Textures/By Asset Type/Power Icons/MarvelMods_Export_Asset_Icons.py
+++ Export Textures/By Asset Type/Power Icons/MarvelMods_Export_Asset_Icons.py
@@ -7,11 +7,12 @@
 # GIMP plugin to export power icons
 # This was designed with the intention to use it with modding processes for MarvelMods.com, though it can have other uses. 
 # For detailed instructions, please reference the README.md file included with this download.
-# (c) BaconWizard17 2023
+# (c) BaconWizard17 2025
 #
 #   History:
 #   v1.0: 01Feb2023: First published version.
 #   v2.0: 12Dec2024: Full redesign for improved performance using an external module for common operations.
+#   v3.0: 16Sep2025: Reduced options, as all texture format conversion and resizing can now be done through ALchemy.
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -33,120 +34,70 @@
 # ####### #
 # GIMP module
 from gimpfu import *
-# Marvel Mods Operations
-import Marvel_Mods_Basic_Gimp_Procedures as MMBGP
+# Internal modules
+import marvel_mods_basic_gimp_procedures as mmbgp
+# External modules
+from datetime import datetime
 
 
 # ######### #
 # FUNCTIONS #
 # ######### #
-# Define the main operation
-def exportIcons(image, layer, console, game):
-    # Perform the initial operations
-    (okayToExport, xcfPath) = MMBGP.initialOps(image, layer, checkSquare=True)
-    # Verify that it's okay to export
-    if okayToExport == True:
-        # It's okay to export
-        # Export a plain png copy as a preview
-        MMBGP.exportTextureMM(image, layer, xcfPath, ".png", transparent=True, subFolder="!Preview")
-        # Pick the console
-        if game == 0:
-            # XML1
-            # Determine if the image is oversized (only check width because it was confirmed to be square)
-            if image.width > 128:
-                # The image is oversized, so set the scaleFactor
-                scaleFactor = 128 / float(image.width)
-            else:
-                # No scaling is needed
-                scaleFactor = 1
-            # Export the image
-            MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, indexColors=256, subFolder="XML1 All")
-        elif game == 1:
-            # XML2
-            # Determine if an icons2 file is needed
-            if image.width > 128:
-                icons2 = True
-            else:
-                icons2 = False
-            # Decide what icons are needed
-            if icons2 == True:
-                # Export icons2
-                # Pick the console
-                if console == 1:
-                    # PC Only
-                    # Determine if the icons are high-res
-                    if image.width > 256:
-                        # 512x512 icons, export the PC as a dds
-                        MMBGP.exportTextureMM(image, layer, xcfPath, ".dds", fileNameSuffix="2", subFolder="XML2 PC")
-                    else:
-                        # 256x256 icons, export as PNG8
-                        MMBGP.exportTextureMM(image, layer, xcfPath, ".png", indexColors=256, fileNameSuffix="2", subFolder="XML2 PC")
-                    # Get the scale factor for the icons1 file
-                    scaleFactor = 128 / float(image.width)
-                    # Export the image
-                    MMBGP.exportTextureMM(image, layer, xcfPath, ".png", indexColors=256, fileNameSuffix="1", subFolder="XML2 PC")
-                else:
-                    # All consoles
-                    # Determine if PC needs high res icons
-                    if image.width > 256:
-                        # 512x512 icons, export the PC as a dds
-                        MMBGP.exportTextureMM(image, layer, xcfPath, ".dds", fileNameSuffix="2", subFolder="XML2 PC")
-                        # Get the scale factor for 256x256
-                        scaleFactor = 256 / float(image.width)
-                        # Export the console textures
-                        MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, indexColors=256, fileNameSuffix="2", subFolder="XML2 Xbox")
-                    else:
-                        # 256x256 icons, export the PC with Xbox
-                        # Export the image
-                        MMBGP.exportTextureMM(image, layer, xcfPath, ".png", indexColors=256, fileNameSuffix="2", subFolder="XML2 PC and Xbox")
-                    # Get the scale factor for 128x128
-                    scaleFactor = 128 / float(image.width)
-                    # Export the image
-                    MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, indexColors=256, fileNameSuffix="1", subFolder="XML2 All")
-            else: 
-                # Do not export icons2
-                # Determine the console
-                if console == 1:
-                    # PC only
-                    # Export the image
-                    MMBGP.exportTextureMM(image, layer, xcfPath, ".png", indexColors=256, fileNameSuffix="1", subFolder="XML2 PC")
-                else:
-                    # All consoles
-                    # Export the image
-                    MMBGP.exportTextureMM(image, layer, xcfPath, ".png", indexColors=256, fileNameSuffix="1", subFolder="XML2 All")
-        elif game == 2:
-            # MUA1
-            # Determine if the image is oversized
-            if image.width > 256:
-                scaleFactor = 256 / float(image.width)
-            else:
-                scaleFactor = 1
-            # Determine the console
-            if console == 0:
-                # All
-                # Export the image
-                MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA1 PC, Steam, PS3, and 360")
-                # Get the scale factor for 128x128
-                scaleFactor = 128 / float(image.width)
-                # Export the last-gen textures
-                MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA1 Wii and Xbox")
-                MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA1 PS2 and PSP", alphaIndexed=True)
-            else:
-                # PC only
-                # Export the image
-                MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA1 PC and Steam")   
+# This is the main operation.
+def ExportIcons(image, layer, console, game):
+    # Perform the initial operations.
+    (okay_to_export, xcf_path) = mmbgp.initialOps(image, layer, check_square = True)
+    # Verify that it's okay to export.
+    if okay_to_export == True:
+        # It's okay to export.
+        # Set the transparency based on the game.
+        if game in ['XML1', 'XML2']:
+            transparency = False
         else:
-            # MUA2
-            # Determine if the image is oversized
+            transparency = True
+        # Export a plain png copy as a preview.
+        mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', file_name_prefix = '!Preview - ', transparent = transparency)
+        # Pick the console.
+        if game == 0:
+            # XML1.
+            # Determine if the image is oversized (only check width because it was confirmed to be square).
             if image.width > 128:
-                scaleFactor = 128 / float(image.width)
+                # The image is oversized, so set the scale_factor_set.
+                scale_factor_set = 128 / float(image.width)
             else:
-                scaleFactor = 1
-            # Export the image
-            MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA2 Wii")
-            MMBGP.exportTextureMM(image, layer, xcfPath, ".png", scale_factor=scaleFactor, transparent=True, subFolder="MUA2 PS2 and PSP", alphaIndexed=True)
-        # Print the success message
-        pdb.gimp_message("SUCCESS: exported " + xcfPath)
+                # No scaling is needed.
+                scale_factor_set = 1
+            # Export the image.
+            mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', scale_factor = scale_factor_set, file_name_prefix = 'XML1_')
+        elif game == 1:
+            # XML2.
+            # Determine if an icons2 file is needed.
+            if image.width > 128:
+                # An icons2 file is needed.
+                # Export the icons2 file.
+                mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', file_name_prefix = 'XML2_', file_name_suffix = '2')
+            # Export the icons1 file.
+            mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', file_name_prefix = 'XML2_', file_name_suffix = '1', scale_factor = 128 / float(image.width))
+        elif game == 2:
+            # MUA1.
+            # Determine if the image is oversized.
+            if image.width > 256:
+                scale_factor_set = 256 / float(image.width)
+            else:
+                scale_factor_set = 1
+            # Export the image.
+            mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', scale_factor = scale_factor_set, file_name_prefix = 'MUA1_', transparent = True)
+        else:
+            # MUA2.
+            # Determine if the image is oversized.
+            if image.width > 128:
+                scale_factor_set = 128 / float(image.width)
+            else:
+                scale_factor_set = 1
+            # Export the image.
+            mmbgp.ExportTextureMM(image, layer, xcf_path, '.png', scale_factor = scale_factor_set, file_name_prefix = 'MUA2_', transparent = True)
+        # Print the success message.
+        pdb.gimp_message('SUCCESS: exported ' + xcf_path + ' at ' + str(datetime.now().strftime('%H:%M:%S')))
 
 
 # ######## #
@@ -154,23 +105,22 @@ def exportIcons(image, layer, console, game):
 # ######## #
 # Register the script in GIMP
 register(
-    "python_fu_marvelmods_export_asset_icons",
-    "Exports a power icons texture in multiple formats.\n\nCheck the README.md file included with the\ndownload for more clarity on the options.",
-    "Exports a power icons texture in multiple formats.",
-    "BaconWizard17",
-    "BaconWizard17",
-    "December 2024",
-    "Export Power Icons",
-    "*",
+    'python_fu_marvelmods_export_asset_icons',
+    'Exports a power icons texture.\n\nCheck the README.md file included with the\ndownload for more clarity on the options.',
+    'Exports a power icons texture.',
+    'BaconWizard17',
+    'BaconWizard17',
+    'December 2024',
+    'Export Power Icons',
+    '*',
     [
-        (PF_IMAGE, "image", "Input image", None),
-        (PF_DRAWABLE, "layer", "Layer, mask or channel", None),
-        (PF_OPTION, "console", "Console:", 0, ["All", "PC Only"]),
-        (PF_OPTION, "game", "Game:", 0, ["XML1", "XML2", "MUA1", "MUA2"])
+        (PF_IMAGE, 'image', 'Input image', None),
+        (PF_DRAWABLE, 'layer', 'Layer, mask or channel', None),
+        (PF_OPTION, 'game', 'Game:', 0, ['XML1', 'XML2', 'MUA1', 'MUA2'])
     ],
     [],
-    exportIcons,
-    menu="<Image>/Marvel Mods/Export Textures/By Asset Type"
+    ExportIcons,
+    menu='<Image>/Marvel Mods/Export Textures/By Asset Type'
 )
 
 
